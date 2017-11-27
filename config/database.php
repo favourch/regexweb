e68673da03f7f5941993c4463cfee2f8ceb05d6f
SenseiFavour <fchukwuedo@gmail.com>
2017-11-27 01:21:37 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'openfamilychurch.org.ng'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'openfami_regex'),
+            'username' => env('DB_USERNAME', 'openfami_regex'),
+            'password' => env('DB_PASSWORD', 'ncsi24%csimiamI'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
@@ -68,11 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'openfamilychurch.org.ng'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'openfami_regex'),
+            'username' => env('DB_USERNAME', 'openfami_regex'),
+            'password' => env('DB_PASSWORD', 'ncsi24%csimiamI'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
